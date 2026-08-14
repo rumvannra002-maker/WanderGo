531a8db39829a56afe25c53d53e9f222d56bdec1
Rum Vannra <rumvannra002@gmail.com>
2026-08-14 15:44:59 +0700

    Add basename to BrowserRouter in App.jsx

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/WanderGo/', // ត្រូវដាក់វានៅទីនេះ (ក្រៅ plugins)
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
-})
+  base: '/WanderGo/',
+  plugins: [react()],
+});
