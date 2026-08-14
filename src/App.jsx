1fecfe1ddbdd9516305c8228180b838c29903cfa
Rum Vannra <rumvannra002@gmail.com>
2026-08-14 16:22:29 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Contact from './pages/Contact';
 
 export default function App() {
   return (
-    <BrowserRouter basename="/WanderGo">
+    <BrowserRouter basename="/WanderGo_website/">
       <Navbar />
       <main>
         <Routes>
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/WanderGo/',
+  base: '/WanderGo_website/',
   plugins: [react()],
 });
