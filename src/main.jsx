da1b1dbd736f52c2fa2d3fffb8cbf9e4d234198c
Rum Vannra <rumvannra002@gmail.com>
2026-08-14 15:59:02 +0700

    Fix router basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <LanguageProvider>
       <AuthProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/WanderGo/">
           <App />
         </BrowserRouter>
       </AuthProvider>
